c1c6c0ac7c853d31b2c7ede235b0d7c8d15f23d7
Mustapher Matias <mustaphermatias@gmail.com>
2020-03-26 19:47:35 +0200

    REFACTORING THE BIKESHARE CODES     Making each dictionary item (the key and the value) on its own Lines

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -3,9 +3,11 @@ import datetime
 import pandas as pd
 import numpy as np
 
-CITY_DATA = { 'chicago': 'chicago.csv',
+CITY_DATA = {
+              'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
+              'washington': 'washington.csv'
+            }
 
 def get_filters():
     """
@@ -40,7 +42,8 @@ def get_filters():
     if time_filter.lower() == 'month':
         day = "all"
         months = [
-                  'january', 'february', 'march', 'april', 'may', 'june'
+                  'january', 'february', 'march', 'april',
+                  'may', 'june'
                  ]
 
         while True:
@@ -68,7 +71,8 @@ def get_filters():
 
     elif time_filter.lower() == 'both':
         months = [
-                  'january', 'february', 'march', 'april', 'may', 'june'
+                  'january', 'february', 'march', 'april',
+                  'may', 'june'
                  ]
 
         while True:
